4dba6b6cd3c1a97df086f1b5e79c0b488ce5b58d
Robert Horvat <robert.horvath93@gmail.com>
2023-09-07 03:35:53 +0300

    File compressor app

---
+++ Projects/File_compressor/final_zip_creator.py
@@ -0,0 +1,18 @@
+import zipfile
+
+
+import pathlib
+
+
+def make_archive(filepaths, dest_dir):
+    dest_path = pathlib.Path(dest_dir, "compressed.zip")
+    with zipfile.ZipFile(dest_path, "w") as archive:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+
+
+            archive.write(filepath, arcname=filepath.name)
+
+if __name__ == "__main__":
+
+    make_archive(filepaths=["bonus1.py", "feet_inches.py"], dest_dir="dest")
